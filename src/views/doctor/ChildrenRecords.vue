d0a6d45cd2174eb17ad8fc5684ca2df60c1099af
HUSSAMEBOU <hussamabushanab21@gmail.com>
2026-09-12 21:08:42 +0300

    feat(ui): remove add child button from doctor children records

--- src/views/doctor/ChildrenRecords.vue
+++ src/views/doctor/ChildrenRecords.vue
@@ -5,9 +5,6 @@
         <h1>سجلات الأطفال</h1>
         <p class="subtitle">إدارة سجلات الأطفال وحالات التحصين</p>
       </div>
-      <button type="button" class="btn btn-primary" @click="showAddChildModal = true">
-        <i class="ti ti-plus"></i> إضافة طفل جديد
-      </button>
     </div>
 
     <section class="stats-row">
@@ -144,11 +141,6 @@
       @scanned="handleQrScanned"
     />
 
-    <AddChildModalDoctor
-      v-if="showAddChildModal"
-      @close="showAddChildModal = false"
-      @saved="showAddChildModal = false"
-    />
   </DoctorLayout>
 </template>
 
@@ -159,7 +151,6 @@ import { useChildrenStore } from '../../stores/children'
 import { useGuardiansStore } from '../../stores/guardians'
 import ChildQrModal from '../../components/ChildQrModal.vue'
 import QrScannerModal from '../../components/QrScannerModal.vue'
-import AddChildModalDoctor from '../../components/AddChildModalDoctor.vue'
 import { scanChildQrApi } from '../../utils/api'
 
 const childrenStore = useChildrenStore()
@@ -170,7 +161,6 @@ const completingDoseId = ref(null)
 const vaccineError = ref('')
 const searchQuery = ref('')
 const showQrScanner = ref(false)
-const showAddChildModal = ref(false)
 
 async function handleQrScanned(qrCode) {
   showQrScanner.value = false
